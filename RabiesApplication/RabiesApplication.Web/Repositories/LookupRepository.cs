1d9d981e6637ac195d1d40a2c4d2e14565bffc7f
Sagar Jha <sagar.jhaa@gmail.com>
2017-06-26 08:56:53 -0400

    Add lookup repository

---
+++ RabiesApplication/RabiesApplication.Web/Repositories/LookupRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabiesApplication.Models;
+
+namespace RabiesApplication.Web.Repositories
+{
+    public class StatesRepository :BaseRepository<State>
+    {
+        public override Task<State> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task InsertOrUpdateAsync(State model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<State> GetAllStates()
+        {
+            return Context.States.AsQueryable();
+        }
+    }
+}
