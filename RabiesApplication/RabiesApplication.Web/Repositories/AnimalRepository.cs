1809bcac650cff0334f3ea9126a542694b83a2d0
Sagar Jha <sagar.jhaa@gmail.com>
2017-06-28 20:02:51 -0400

    Add Animal Repository

---
+++ RabiesApplication/RabiesApplication.Web/Repositories/AnimalRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabiesApplication.Models;
+
+namespace RabiesApplication.Web.Repositories
+{
+    public class AnimalRepository : AuditRepository<Animal>
+    {
+
+    }
+}
