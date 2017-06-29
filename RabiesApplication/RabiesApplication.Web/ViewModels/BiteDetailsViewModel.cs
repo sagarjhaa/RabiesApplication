fb65af2c9c60dd382ef014c670d6515865b87134
Sagar Jha <sagar.jhaa@gmail.com>
2017-06-28 20:54:02 -0400

    Edit BiteDetailsViewModel

--- RabiesApplication/RabiesApplication.Web/ViewModels/BiteDetailsViewModel.cs
+++ RabiesApplication/RabiesApplication.Web/ViewModels/BiteDetailsViewModel.cs
@@ -10,10 +10,26 @@ namespace RabiesApplication.Web.ViewModels
     public class BiteDetailsViewModel
     {
         public Bite Bite { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
+
+        #region HumanVictim
+
         public IEnumerable<HumanVictim> HumanVictims { get; set; }
         public IEnumerable<State> States { get; set; }
         public IEnumerable<County> Counties { get; set; }
         public IEnumerable<City> Cities { get; set; }
-        public IEnumerable<Employee> Employees { get; set; }
+        
+
+        #endregion
+
+        #region Pet
+
+        public IEnumerable<Animal> Animal { get; set; }
+        public IEnumerable<Breed> Breeds { get; set; }
+        public IEnumerable<Species> Specieses { get; set; }
+        public IEnumerable<Vet> Vets { get; set; }
+
+        #endregion
+
     }
 }
