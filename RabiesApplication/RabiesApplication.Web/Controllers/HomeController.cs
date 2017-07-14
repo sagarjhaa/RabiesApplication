8454d008507047065d6644e9e434822e4535b2d7
Sagar Jha <sagar.jhaa@gmail.com>
2017-07-14 13:57:53 -0400

    Remove index page and action method

--- RabiesApplication/RabiesApplication.Web/Controllers/HomeController.cs
+++ RabiesApplication/RabiesApplication.Web/Controllers/HomeController.cs
@@ -9,11 +9,7 @@ namespace RabiesApplication.Web.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+      
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
