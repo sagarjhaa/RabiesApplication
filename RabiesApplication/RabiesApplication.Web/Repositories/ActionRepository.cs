129a221c00fea9a861cdf7d55c32d7c3e12496da
Sagar Jha <sagar.jhaa@gmail.com>
2017-07-20 10:41:48 -0400

    Add Action Repository

---
+++ RabiesApplication/RabiesApplication.Web/Repositories/ActionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Action = RabiesApplication.Models.Action;
+
+namespace RabiesApplication.Web.Repositories
+{
+    public  class ActionRepository : ActiveRepository<Action>
+    {
+    }
+}
