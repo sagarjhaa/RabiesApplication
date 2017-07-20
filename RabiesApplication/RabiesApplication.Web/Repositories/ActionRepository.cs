be51a336cba7ad8934c582642e8bdd316a7cdec1
Sagar Jha <sagar.jhaa@gmail.com>
2017-07-20 12:38:01 -0400

    Add method to see all actions by biteId

--- RabiesApplication/RabiesApplication.Web/Repositories/ActionRepository.cs
+++ RabiesApplication/RabiesApplication.Web/Repositories/ActionRepository.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using RabiesApplication.Web.Models;
 using Action = RabiesApplication.Models.Action;
 
 namespace RabiesApplication.Web.Repositories
 {
     public class ActionRepository : ActiveRepository<Action>
     {
+        public IEnumerable<Action> GetActionsByBiteId(string biteId)
+        {
+            if (biteId == null)
+            {
+                return null;
+            }
+
+            return All().Where(a=> a.Active.Equals(Constant.Active)).Where(a => a.BiteId.Equals(biteId)).OrderBy(a => a.RecordCreated);
+        }
     }
 }
