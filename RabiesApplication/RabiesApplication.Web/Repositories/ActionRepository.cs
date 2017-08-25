06da997dfb5dc88ea3bd11343953a90616c58dca
Sagar Jha <sagar.jhaa@gmail.com>
2017-08-25 14:55:09 -0400

    Add ActionDTo to manipulate the action retured with url

--- RabiesApplication/RabiesApplication.Web/Repositories/ActionRepository.cs
+++ RabiesApplication/RabiesApplication.Web/Repositories/ActionRepository.cs
@@ -10,16 +10,44 @@ using Action = RabiesApplication.Models.Action;
 
 namespace RabiesApplication.Web.Repositories
 {
+    public class ActionDTO
+    {
+        public string Id { get; set; }
+        public string BiteId { get; set; }
+        public string ActionType { get; set; }
+        public string Comments { get; set; }
+        public string DocumentId { get; set; }
+    }
+
+
     public class ActionRepository : ActiveRepository<Action>
     {
-        public IEnumerable<Action> GetActionsByBiteId(string biteId)
+        public IEnumerable<ActionDTO> GetActionsByBiteId(string biteId)
         {
             if (biteId == null)
             {
                 return null;
             }
+            return  (from a in Context.Actions
+                where a.Active.Equals(Constant.Active)
+                where a.BiteId.Equals(biteId)
+                orderby a.RecordCreated
+                select new ActionDTO()
+                {
+                    Id = a.Id,
+                    BiteId = a.BiteId,
+                    DocumentId = a.DocumentId ,//== null ? a.DocumentId: a.DocumentId,
+                    ActionType = a.ActionType,
+                    Comments = a.Comments
+                }).ToList();
+            
+            //return All().Where(a=> a.Active.Equals(Constant.Active))
+            //                .Where(a => a.BiteId.Equals(biteId))
+            //                .OrderBy(a => a.RecordCreated);
 
-            return All().Where(a=> a.Active.Equals(Constant.Active)).Where(a => a.BiteId.Equals(biteId)).OrderBy(a => a.RecordCreated);
+
+            
+            
         }
     }
 }
