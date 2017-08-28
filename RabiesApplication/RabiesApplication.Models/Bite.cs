d48a7676724dcac843587ef6cfc6d49da5cc6ec2
Sagar Jha <sagar.jhaa@gmail.com>
2017-08-28 15:02:09 -0400

    Display Names in bite table

--- RabiesApplication/RabiesApplication.Models/Bite.cs
+++ RabiesApplication/RabiesApplication.Models/Bite.cs
@@ -12,12 +12,14 @@ namespace RabiesApplication.Models
         public byte[] RowVersion { get; set; }
 
         //Which city the bite occured
+        [DisplayName("City")]
         public string CityId { get; set; }
 
         public  City City { get; set; }
 
         //What state the bite occured
         //It will be Ohio for us
+        [DisplayName("State")]
         public string StateId { get; set; }
 
         public  State State { get; set; }
@@ -26,17 +28,20 @@ namespace RabiesApplication.Models
         [Required]
         //When bite happened
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}",ApplyFormatInEditMode = true)]
+        [DisplayName("Bite Date")]
         public DateTimeOffset? BiteDate { get; set; }
 
         [Required]
         //When it was reported to us
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}",ApplyFormatInEditMode = true)]
+        [DisplayName("Bite Report Date")]
         public DateTimeOffset? BiteReportDate { get; set; }
 
         [Required]
         //Where did we get this information from
+        [DisplayName("Bite Reported By")]
         public string BiteReportedBy { get; set; }
-
+        [DisplayName("Bite Status")]
         public string BiteStatusId { get; set; }
 
         public  BiteStatus BiteStatus { get; set; }
@@ -44,13 +49,13 @@ namespace RabiesApplication.Models
         public string Comments { get; set; }
 
         //public DateTimeOffset? InvestigationCompletionDate { get; set; }
-        
+        [DisplayName("Employee Assigned")]
         public string EmployeeAssignedId { get; set; }
         
         public bool Active { get; set; }
-
+        [DisplayName("Investigation Complete Date")]
         public DateTime? InvestigationCompletionDate { get; set; }
-
+        [DisplayName("Report Closed Date")]
         public DateTime? ReportClosedDate { get; set; }
 
         public DateTimeOffset RecordCreated { get; set; }
