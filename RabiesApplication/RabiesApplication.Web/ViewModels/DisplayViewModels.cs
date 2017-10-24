232e8b30f8fea8f9fcf718c3841386ab3643b192
Sagar Jha <sagar.jhaa@gmail.com>
2017-10-24 08:07:39 -0400

    Create two folders FormViewModels and DisplayViewModels
    For Form Views and Display Views

--- RabiesApplication/RabiesApplication.Web/ViewModels/DisplayViewModels.cs
+++ RabiesApplication/RabiesApplication.Web/ViewModels/DisplayViewModels.cs
@@ -44,6 +44,17 @@ namespace RabiesApplication.Web.ViewModels
 
     }
 
+    public class HumanVictimFormViewModel
+    {
+        public string Id { get; set; }
+        public string BiteId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get;set; }
+    }
+
+
     public class AnimalViewModel
     {
         public string Id { get; set; }
