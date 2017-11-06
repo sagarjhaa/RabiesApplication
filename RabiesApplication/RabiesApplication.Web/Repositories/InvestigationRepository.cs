1f385d49f1493b93dac8a4cb8efe84357a903ec4
Sagar Jha <sagar.jhaa@gmail.com>
2017-11-06 09:16:52 -0500

    Add Investigation Repository

---
+++ RabiesApplication/RabiesApplication.Web/Repositories/InvestigationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabiesApplication.Models;
+using RabiesApplication.Models.Interfaces;
+
+namespace RabiesApplication.Web.Repositories
+{
+    public class InvestigationRepository : AuditRepository<Investigation>
+    {
+    }
+}
